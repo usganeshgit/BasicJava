f7df0117f17700f8c143ee4e1b42cbf0a5d12859
Nitish Lonka <nitishlonka13@gmail.com>
2023-04-21 20:21:54 -0400

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,48 @@
+
+public class Calculator {
+
+	//Declare two variables here
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      if(sum(6,3)==9)
+      {
+    	  System.out.print("Sum Executed successfully");
+      }
+      if(multiply(6,3)==18)
+      {
+    	  System.out.print("Multiply Executed successfully");
+      }
+      if(division(6,3)==2)
+      {
+    	  System.out.print("division Executed successfully");
+      }
+      if(subtract(6,3)==3)
+      {
+    	  System.out.print("Subtract Executed successfully");
+      }
+	}
+	
+	//Pass two variables into the below method and return sum
+	public static int sum( ) {
+		
+	}
+	
+	//Pass two variables into the below method and return product
+	public static int multiply() {
+		
+	}
+	
+	//Pass two variables into the below method and return quotient
+	public static int division() {
+		
+	}
+	
+	//Pass two variables into the below method and return difference
+	public static int subtract() {
+		
+	}
+
+}
